dec1c08635a198cd0789b447c08b3b6b7e88ca0b
Tiago Silva <tiagosilva_live_2016@outlook.com>
2025-01-20 17:30:04 -0300

    adicionado id da api e o throw

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const texto = document.getElementById('descricao')
+const numeroDoConselho = document.getElementById('numero-conselho')
+
 document.getElementById('gerar-conselho').addEventListener('click', () => {
     buscandoConselhoAleatorio()
     
@@ -6,8 +9,12 @@ document.getElementById('gerar-conselho').addEventListener('click', () => {
         try {
             const url = "https://api.adviceslip.com/advice"
             const resposta = await fetch(url)
-            const json = await resposta.json()
-            return document.getElementById('descricao').innerText = json.slip.advice
+            if(!resposta.ok){
+                throw new error('Deu erro na busca do conselho da API')
+            }
+            const json = await resposta.json()         
+            return texto.innerText = json.slip.advice,
+            numeroDoConselho.innerText = json.slip.id
         } catch (error) {
             console.log(error);
         }
